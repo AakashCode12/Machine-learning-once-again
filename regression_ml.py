15e6505a70c5e3ef1700d6cf4de9cfa8652d2307
AakashCode12 <aakashcoder1210@gmail.com>
2021-01-11 21:24:09 +0530

    tip added

--- regression_ml.py
+++ regression_ml.py
@@ -3,3 +3,14 @@ import quandl
 
 df = quandl.get('WIKI/GOOGL')
 print(df.head())
+
+# tip learned:
+
+'''
+so now, we have many columns so we need to remove the unwanted columns in data
+
+q: how to find the unwanted columns ??
+
+Ans: If the correlation between them is high, then most probably they will be interdependent and hence we can remove one of them
+
+'''
